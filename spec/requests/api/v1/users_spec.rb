231b422b96a9a680d80aa6e257758c8bfa40c50b
Marco <m_uno_p@hotmail.com>
2025-02-23 12:13:32 -0500

    Add the user create endpoint

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -13,4 +13,36 @@ RSpec.describe "Api::V1::Users", type: :request do
       expect(json_response["email"]).to eq(user.email)
     end
   end
+
+  describe "POST /api/v1/users" do
+    context "when valid parameters are provided" do
+      it "creates a new user" do
+        expect {
+          post api_v1_users_url, params: {
+            user: {
+              email: "test@test.org",
+              password: "123456"
+            }
+          }, as: :json
+        }.to change(User, :count).by(1)
+
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    context "when email is already taken" do
+      it "does not create a new user" do
+        expect {
+          post api_v1_users_url, params: {
+            user: {
+              email: user.email,
+              password: "123456"
+            }
+          }, as: :json
+        }.not_to change(User, :count)
+
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
 end
